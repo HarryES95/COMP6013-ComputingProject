a21462ede9ea19f5e9e8685d86e5f0a7a48de8cb
Harry Stout <19044035@brookes.ac.uk>
2022-03-19 05:39:00 +0000

    Change code to OO style, introduce GUI

--- condaEnv/ChaplinCNN.py
+++ condaEnv/ChaplinCNN.py
@@ -6,7 +6,7 @@ import skvideo.io as svio
 import numpy as np
 from scenedetect import VideoManager
 from scenedetect import SceneManager
-
+import time
 MODEL_HEIGHT = 160
 MODEL_WIDTH = 320
 
@@ -33,6 +33,7 @@ def formatAndSaveNumpyData(numpy_video,scene_list,split):
     data = (x for x in numpy_video.nextFrame()) #Use generator so entire file is not loaded into memory
     hFactor,wFactor,hTPad,hBPad,wLPad,wRPad = split
     size = 0
+    test = (numpy_video.inputframenum*hFactor*wFactor)-(len(scene_list)*7)
     for scene in scene_list:
         count = scene[0].frame_num   
         limit = scene[1].frame_num
@@ -54,7 +55,7 @@ def formatAndSaveNumpyData(numpy_video,scene_list,split):
             label,sample = np.reshape(label,(hFactor*wFactor,int(numpy_video.outputheight/hFactor),int(numpy_video.outputwidth/wFactor),3)),np.reshape(sample,(hFactor*wFactor,int(numpy_video.outputheight/hFactor),int(numpy_video.outputwidth/wFactor),6))
             label,sample = np.moveaxis(label,-1,1),np.moveaxis(sample,-1,1)
             #pad
-            label,sample = np.pad(label,((0,0),(0,0),(hTPad,hBPad),(wLPad,wRPad)),mode="empty"),np.pad(sample,((0,0),(0,0),(hTPad,hBPad),(wLPad,wRPad)),mode="empty")
+            label,sample = np.pad(label,((0,0),(0,0),(hTPad,hBPad),(wLPad,wRPad)),mode="constant"),np.pad(sample,((0,0),(0,0),(hTPad,hBPad),(wLPad,wRPad)),mode="constant")
             #Save sections to the disk
             labelsFile.write(np.ascontiguousarray(label))
             samplesFile.write(np.ascontiguousarray(sample))
@@ -121,7 +122,7 @@ def buildDataset(size):
     return dataset
 
 def enhance(numpy_video, model, split):
-    writer = svio.FFmpegWriter("E:\\Results\\enhanced.mp4", inputdict={
+    writer = svio.FFmpegWriter("E:\\Results\\FloorEnhanced.mp4", inputdict={
       '-r': "50",
     },
     outputdict={
@@ -141,7 +142,7 @@ def enhance(numpy_video, model, split):
         quadrants = [np.moveaxis(q,-1,1) for q in [y for y in quadrants]]
         quadrants = np.reshape(quadrants,(hFactor*wFactor,6,int(numpy_video.outputheight/hFactor),int(numpy_video.outputwidth/wFactor)))
         #pad
-        quadrants = np.pad(quadrants,( (0,0),(0,0),(hTPad,hBPad),(wRPad,wLPad)),mode="empty")
+        quadrants = np.pad(quadrants,( (0,0),(0,0),(hTPad,hBPad),(wRPad,wLPad)),mode="constant")
         predict = model.predict(tf.cast(quadrants,tf.float16)/255.)
         #crop
         predict = (predict*255.).astype(np.uint8)
@@ -162,21 +163,21 @@ def enhance(numpy_video, model, split):
     return
 
 def loss(y,y_pred,e=0.001):
-    return tf.math.sqrt(tf.math.square(y-y_pred)+tf.math.square(e))
+    return tf.math.sqrt(tf.math.square(y-y_pred+tf.math.square(e)))
 
 def ssim(y_true,y_pred):
     return tf.reduce_mean(tf.image.ssim(tf.transpose(y_true, perm=[0,2,3,1]),tf.transpose(y_pred, perm=[0,2,3,1]),1.0))
 
-size = 503140
-videoFilePath = "Dataset\\output.mp4"
+#size = 503140
+videoFilePath = "E:\\Results\\cureEnhanced.mp4"
 numpy_video = loadVideo(videoFilePath)
 split = calculateSplitFactor(numpy_video.outputheight,numpy_video.outputwidth)
-#scene_list = split_scenes(numpy_video,videoFilePath) #Split video so that 'cuts' don't interfere with formatting dataset
-#size = formatAndSaveNumpyData(numpy_video,scene_list,split)*split[0]*split[1]
+scene_list = split_scenes(numpy_video,videoFilePath) #Split video so that 'cuts' don't interfere with formatting dataset
+size = formatAndSaveNumpyData(numpy_video,scene_list,split)*split[0]*split[1]
 dataset = buildDataset(size)
-split_size = int((size/32)*0.8)
-train_data = dataset.take(split_size)
-test_data = dataset.skip(split_size)
+#split_size = int((size/32)*0.8)
+#train_data = dataset.take(split_size)
+#test_data = dataset.skip(split_size)
 
 inputs = layers.Input(shape=(6,160,320))
 conv2d = layers.Conv2D(32, (7,7), activation='relu', padding='same', data_format="channels_first")(inputs)
@@ -207,26 +208,69 @@ conv2d_16 = layers.Conv2D(32, (3, 3), activation='relu', padding='same', data_fo
 conv2d_17 = layers.Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_first")(conv2d_16)
 outputs = layers.Conv2D(3, (1,1), activation='sigmoid', padding='same', data_format="channels_first")(conv2d_17)
 
-checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    filepath = "E:\\Model\\checkpoint",
-    monitor = "loss",
-    mode = "min",
-    save_weights_only = True,
-    save_freq = "epoch"
-)
+# inputs = layers.Input(shape=(6,160,320))
+# conv2d = layers.Conv2D(32, (7,7), activation='relu', padding='same', data_format="channels_first")(inputs)
+# max_pooling2d = layers.AveragePooling2D(pool_size=(2, 2), data_format="channels_first")(conv2d)
+# conv2d_2 = layers.Conv2D(64, (5, 5), activation='relu', padding='same', data_format="channels_first")(max_pooling2d)
+# max_pooling2d_1 = layers.AveragePooling2D(pool_size=(2, 2), data_format="channels_first")(conv2d_2)
+# conv2d_4 = layers.Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_first")(max_pooling2d_1)
+# max_pooling2d_2 = layers.AveragePooling2D(pool_size=(2, 2), data_format="channels_first")(conv2d_4)
+# conv2d_6 = layers.Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_first")(max_pooling2d_2)
+# max_pooling2d_3 = layers.AveragePooling2D(pool_size=(2, 2), data_format="channels_first")(conv2d_6)
+# conv2d_8 = layers.Conv2D(512, (3, 3), activation='relu', padding='same', data_format="channels_first")(max_pooling2d_3)
+# up_sampling2d = layers.Concatenate(axis=1)([layers.UpSampling2D(size=(2, 2), data_format="channels_first")(conv2d_8),conv2d_6])
+# conv2d_10 = layers.Conv2D(256, (3, 3), activation='relu', padding='same', data_format="channels_first")(up_sampling2d)
+# up_sampling2d_1 = layers.Concatenate(axis=1)([layers.UpSampling2D(size=(2, 2), data_format="channels_first")(conv2d_10),conv2d_4])
+# conv2d_12 = layers.Conv2D(128, (3, 3), activation='relu', padding='same', data_format="channels_first")(up_sampling2d_1)
+# up_sampling2d_2 = layers.Concatenate(axis=1)([layers.UpSampling2D(size=(2,2), data_format="channels_first")(conv2d_12),conv2d_2])
+# conv2d_14 = layers.Conv2D(64, (3, 3), activation='relu', padding='same', data_format="channels_first")(up_sampling2d_2)
+# up_sampling2d_3 = layers.Concatenate(axis=1)([layers.UpSampling2D(size=(2,2), data_format="channels_first")(conv2d_14),conv2d])
+# conv2d_16 = layers.Conv2D(32, (3, 3), activation='relu', padding='same', data_format="channels_first")(up_sampling2d_3)
+# outputs = layers.Conv2D(3, (1,1), activation='sigmoid', padding='same', data_format="channels_first")(conv2d_16)
+
+# checkpoint = tf.keras.callbacks.ModelCheckpoint(
+#     filepath = "E:\\Model\\checkpoint",
+#     monitor = "loss",
+#     mode = "min",
+#     save_weights_only = True,
+#     save_freq = "epoch"
+# )
+
+class Progress(tf.keras.callbacks.Callback):
+
+    def __init__(self,model):
+        super(Progress,self).__init__()
+
+    def on_train_batch_end(self,batch,logs=None):
+        print(batch)
 
 model = models.Model(inputs=inputs, outputs=outputs, name="CNN")
 
 model.compile(optimizer=optimizers.Adam(learning_rate=0.0001),
               loss=loss,
-              metrics=[ssim])
+              metrics=ssim)
 
-model.load_weights("E:\\Model\\checkpoint")
+#model.load_weights("E:\\Model\\checkpoint")
+#model.save('E:\\Model\\ChaplinCNN_Test')
+history = model.fit(dataset,epochs=5,callbacks=[Progress(model)])
 
-history = model.fit(train_data,epochs=5,callbacks=[checkpoint])
-
-test = model.evaluate(test_data)
-
+#test = model.evaluate(test_data)
+#test = model.evaluate(dataset)
 #enhance(numpy_video,model,split)
 
-print(test)
+#print(test)
+
+def speedTest(dataset,split):
+    data = dataset.skip(50).take(1).get_single_element()
+    samples = data[0]
+    labels = data[1]
+    sample = tf.stack(tf.squeeze(tf.split(samples,32)[0:split[0]*split[1]]))
+    start = time.time()
+    predict = model.predict(sample)
+    end = time.time()
+    elapsed = end - start
+    print(elapsed)
+
+#speedTest(dataset,split)
+
+print("Pause")
